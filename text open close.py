cb4d0bd20e721a9fe33b8db14db2cd9c3fffb56a
jectrafts <chris.joe.m2@gmail.com>
2025-06-25 19:50:15 +0530

    Add files via upload

--- text open close.py
+++ text open close.py
@@ -38,36 +38,99 @@ with mp_hands.Hands(
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
+
+                zero_landmark = hand_landmarks.landmark[0]
+                x0 = zero_landmark.x
+                y0 = zero_landmark.y
+
+                one_landmark = hand_landmarks.landmark[1]
+                x1 = one_landmark.x
+                y1 = one_landmark.y
+
+                two_landmark = hand_landmarks.landmark[2]
+                x2 = two_landmark.x
+                y2 = two_landmark.y
+
+                three_landmark = hand_landmarks.landmark[3]
+                x3 = three_landmark.x
+                y3 = three_landmark.y
+
+                four_landmark = hand_landmarks.landmark[4]
+                x4 = four_landmark.x
+                y4 = four_landmark.y
+
                 five_landmark = hand_landmarks.landmark[5]
                 x5 = five_landmark.x
                 y5 = five_landmark.y
-                
-                seven_landmark = hand_landmarks.landmark[8]
-                x8 = seven_landmark.x
-                y8 = seven_landmark.y
-                
-                closex = x8 - x5
-                closey = y8 - y5
+
+                six_landmark = hand_landmarks.landmark[6]
+                x6 = six_landmark.x
+                y6 = six_landmark.y
+
+                seven_landmark = hand_landmarks.landmark[7]
+                x7 = seven_landmark.x
+                y7 = seven_landmark.y
+
+                eight_landmark = hand_landmarks.landmark[8]
+                x8 = eight_landmark.x
+                y8 = eight_landmark.y
+
+                nine_landmark = hand_landmarks.landmark[9]
+                x9 = nine_landmark.x
+                y9 = nine_landmark.y
+
+                ten_landmark = hand_landmarks.landmark[10]
+                x10 = ten_landmark.x
+                y10 = ten_landmark.y
+
+                eleven_landmark = hand_landmarks.landmark[11]
+                x11 = eleven_landmark.x
+                y11 = eleven_landmark.y
+
+                twelve_landmark = hand_landmarks.landmark[12]
+                x12 = twelve_landmark.x
+                y12 = twelve_landmark.y
+
+                thirteen_landmark = hand_landmarks.landmark[13]
+                x13 = thirteen_landmark.x
+                y13 = thirteen_landmark.y
+
+                fourteen_landmark = hand_landmarks.landmark[14]
+                x14 = fourteen_landmark.x
+                y14 = fourteen_landmark.y
+
+                fifteen_landmark = hand_landmarks.landmark[15]
+                x15 = fifteen_landmark.x
+                y15 = fifteen_landmark.y
+
+                sixteen_landmark = hand_landmarks.landmark[16]
+                x16 = sixteen_landmark.x
+                y16 = sixteen_landmark.y
+
                 seventeen_landmark = hand_landmarks.landmark[17]
                 x17 = seventeen_landmark.x
                 y17 = seventeen_landmark.y
-                
+
+                eighteen_landmark = hand_landmarks.landmark[18]
+                x18 = eighteen_landmark.x
+                y18 = eighteen_landmark.y
+
+                nineteen_landmark = hand_landmarks.landmark[19]
+                x19 = nineteen_landmark.x
+                y19 = nineteen_landmark.y
+
                 twenty_landmark = hand_landmarks.landmark[20]
                 x20 = twenty_landmark.x
                 y20 = twenty_landmark.y
-                
-                closex1 = x8 - x5
-                closex2 = x17 - x20
-                closey1 = y8 - y5
-                closey2 = y20 - y17
-                if closey1 >= 0.01 and closey2 >= 0.01:
+
+                if y8>y5 and y12>y9 and y16>y13 and y20>y17:
                     print('close')
-                    text='close'
+                    text='bye'
                     hand_state == 0
 
-                else:
+                if y8<y5 and y12<y9 and y16<y13 and y20<y17:
                     print('open')
-                    text='open'
+                    text='hi'
                     hand_state == 1
         font = cv2.FONT_HERSHEY_SIMPLEX
             
