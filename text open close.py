55389f65bb0aa978f47e5e9fde665bfc776015c1
jectrafts <chris.joe.m2@gmail.com>
2025-06-25 18:30:38 +0530

    Add files via upload

---
+++ text open close.py
@@ -0,0 +1,55 @@
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+
+    if not success:
+      print("Ignoring empty camera frame.")
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+    # Flip the image horizontally for a selfie-view display.
+    flipped_image = cv2.flip(image, 1)
+
+    # Add text to the flipped frame
+    text = "Press 'Esc' to exit"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_color = (255, 255, 255)  # White
+    thickness = 2
+    position = (50, 50)  # (x, y)
+    cv2.putText(flipped_image, text, position, font, font_scale, font_color, thickness)
+
+    # Display the frame with flipped text
+    cv2.imshow('MediaPipe Hands', flipped_image)
+
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
